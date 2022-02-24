55acfb85095251b811f3356127aa39f0fd6b1f59
Cesar Torres <torres-cesar@outlook.com>
2022-02-24 12:40:08 -0600

    Create new object for The Hobbit

--- js/script.js
+++ js/script.js
@@ -3,4 +3,15 @@ function Book(title, author, pageCount, hasRead) {
     this.author = author;
     this.pageCount = pageCount;
     this.hasRead = hasRead;
+    if (this.hasRead) {
+        readStatus = "has read";
+    } else if (!this.hasRead) {
+        readStatus = "not read yet";
+    }
+    this.info = function() {
+        console.log(title + ' by ' + author + ', ' + pageCount + ' pages, ' + readStatus)
+    }
 }
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, false);
+theHobbit.info()
